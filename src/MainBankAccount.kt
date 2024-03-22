681288737fd653abb501a1d099f681430d3e6c09
Abrar Karim <md.karimabrar@gmail.com>
2024-03-22 22:30:42 +0600

    Main Function for the project created!

--- src/MainBankAccount.kt
+++ src/MainBankAccount.kt
@@ -1,3 +1,38 @@
+import com.sun.jdi.IntegerValue
+
 fun main() {
-    println("Will be starting Tomorrow!!")
+
+    var accountType: String? = ""
+    var userChoice: Int? = 0
+
+    println("Welcome to your banking system.")
+    println("What type of account would you like to create?")
+
+    println("1. Debit account")
+    println("2. Credit account")
+    println("3. Checking account")
+
+    while (accountType == "") {
+
+        println("Choose an option (1, 2 or 3)")
+
+
+        userChoice = (1..5).random()
+
+        println("The selected option is $userChoice.")
+
+        when (userChoice) {
+
+            1 -> accountType = "debit"
+            2 -> accountType = "credit"
+            3 -> accountType = "checking"
+            else -> continue
+        }
+    }
+
+    println("You have created a $accountType account")
+
 }
+
+
+
