d2c9c80ed01f9051569a90fa59ee9c317db72e83
Abrar Karim <md.karimabrar@gmail.com>
2024-03-23 00:34:25 +0600

    Project Completed. Let's go to Bank!

--- src/MainBankAccount.kt
+++ src/MainBankAccount.kt
@@ -6,8 +6,6 @@ fun main() {
     var accountType: String = ""
     var userChoice: Int = 0
 
-
-
     var output: Int = 0
 
     println("Welcome to your banking system.")
@@ -85,7 +83,56 @@ fun main() {
         }
     }
 
+    fun transfer(mode: String){
+        val transferAmount: Int
 
+        when (mode){
+            "withdraw" -> {
+                if (accountType == "debit") {
+                     transferAmount = debitWithDraw(money)
+                }else {
+                    transferAmount = withdraw(money)
+                }
+                println("The amount you withdraw is $transferAmount dollars.")
+            }
+            "deposit" -> {
+                if (accountType == "credit"){
+                    transferAmount = creditDeposit(money)
+                }else {
+                    transferAmount = deposit(money)
+                }
+                println("The amount you deposited $transferAmount dollars.")
+            }
+            else -> return
+        }
+
+    }
+
+    var isSystemOpen = true
+    var option = 0
+
+    while (isSystemOpen == true) {
+        println("What would you like to do?")
+        println("1. Check bank account balance")
+        println("2. Withdraw money")
+        println("3. Deposit money")
+        println("4. Close the system")
+        println("Which option do you choose? (1, 2, 3 or 4)")
+
+        option = (1..5).random()
+        println("The selected option is ${option}.")
+
+        when (option) {
+            1 -> println("The current balance is ${accountBalance} dollars.")
+            2 -> transfer("withdraw")
+            3 -> transfer("deposit")
+            4 -> {
+                isSystemOpen = false
+                println("The system is closed")
+            }
+            else -> continue
+        }
+    }
 
 }
 
